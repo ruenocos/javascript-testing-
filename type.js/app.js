ee6c2c9368400cf11d5f13bde90705bf42df4644
Wessel <wessel@Vincents-iMac.local>
2018-11-02 13:51:10 +0100

    added type.js file

---
+++ type.js/app.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function () {
+  var options = {
+    strings: ["Developer.", "Student.", "Enthousiast."],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 2000,
+    cursorChar: '',
+    loop: true
+  }
+
+  var typed = new Typed("#typed", options);
+})
